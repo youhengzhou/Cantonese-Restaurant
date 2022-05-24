fd0bffe2a4e064f268f569e96bdd69c35573a25a
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-05-24 18:06:23 -0400

    .

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-};
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
+// };
 
-let [first, , second] = restaurant.categories;
-[first, second] = [second, first];
-console.log(first, second);
+// let [first, , second] = restaurant.categories;
+// [first, second] = [second, first];
+// console.log(first, second);
